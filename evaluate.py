62028f7a19577201e717def97131df05d88e26fb
Christian.Larsson91 <chrlar91@gmail.com>
2017-12-11 13:27:19 +0100

    Changes to evaluate root

--- evaluate.py
+++ evaluate.py
@@ -13,7 +13,8 @@ sys.path.append(os.path.join(BASE_DIR, 'models'))
 sys.path.append(os.path.join(BASE_DIR, 'utils'))
 import provider
 import pc_util
-import pdb
+from vispy import scene, visuals, app, gloo, io
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--gpu', type=int, default=0, help='GPU to use [default: GPU 0]')
@@ -36,7 +37,7 @@ if not os.path.exists(DUMP_DIR): os.mkdir(DUMP_DIR)
 LOG_FOUT = open(os.path.join(DUMP_DIR, 'log_evaluate.txt'), 'w')
 LOG_FOUT.write(str(FLAGS)+'\n')
 
-NUM_CLASSES = 4
+NUM_CLASSES = 3
 SHAPE_NAMES = [line.rstrip() for line in \
     open(os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048/shape_names.txt'))]
 
@@ -44,9 +45,9 @@ HOSTNAME = socket.gethostname()
 
 # ModelNet40 official train/test split
 TRAIN_FILES = provider.getDataFiles( \
-    os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048/MTwMisc/trainWithMisc.txt'))
+    os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048/kittiTrain.txt'))
 TEST_FILES = provider.getDataFiles(\
-    os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048/MTwMisc/testWithMisc.txt'))
+    os.path.join(BASE_DIR, 'data/modelnet40_ply_hdf5_2048/kittiTest.txt'))
 
 
 
@@ -86,7 +87,7 @@ def evaluate(num_votes):
            'pred': pred,
            'loss': loss}
 
-    eval_one_epoch(sess, ops, num_votes)
+    predValues = eval_one_epoch(sess, ops, num_votes)
 
 
 def eval_one_epoch(sess, ops, num_votes=1, topk=1):
@@ -98,82 +99,131 @@ def eval_one_epoch(sess, ops, num_votes=1, topk=1):
     total_seen_class = [0 for _ in range(NUM_CLASSES)]
     total_correct_class = [0 for _ in range(NUM_CLASSES)]
     fout = open(os.path.join(DUMP_DIR, 'pred_label.txt'), 'w')
+
+    current_data, orderList = nn.exportData()
+    current_label = np.zeros(len(current_data))
+    current_data = current_data[:,0:NUM_POINT,:]
+    current_label = np.squeeze(current_label)
+    print(current_data.shape)
+
+    file_size = current_data.shape[0]
+    num_batches = file_size // BATCH_SIZE
+    print(file_size)
     predValues = []
-    for fn in range(len(TEST_FILES)):
-        log_string('----'+str(fn)+'----')
-        current_data, current_label = provider.loadDataFile(TEST_FILES[fn])
-        #pdb.set_trace()
-        current_data = current_data[:,0:NUM_POINT,:]
-        current_label = np.squeeze(current_label)
-        #pdb.set_trace()
 
-        file_size = current_data.shape[0]
-        num_batches = file_size // BATCH_SIZE
-        print(file_size)
+    for batch_idx in range(num_batches):
+        start_idx = batch_idx * BATCH_SIZE
+        end_idx = (batch_idx+1) * BATCH_SIZE
+        cur_batch_size = end_idx - start_idx
 
-        for batch_idx in range(num_batches):
-            start_idx = batch_idx * BATCH_SIZE
-            end_idx = (batch_idx+1) * BATCH_SIZE
-            cur_batch_size = end_idx - start_idx
+        # Aggregating BEG
+        batch_loss_sum = 0 # sum of losses for the batch
+        batch_pred_sum = np.zeros((cur_batch_size, NUM_CLASSES)) # score for classes
+        batch_pred_classes = np.zeros((cur_batch_size, NUM_CLASSES)) # 0/1 for classes
+        for vote_idx in range(num_votes):
+            rotated_data = provider.rotate_point_cloud_by_angle(current_data[start_idx:end_idx, :, :],
+                                              vote_idx/float(num_votes) * np.pi * 2)
+            feed_dict = {ops['pointclouds_pl']: rotated_data,
+                         ops['labels_pl']: current_label[start_idx:end_idx],
+                         ops['is_training_pl']: is_training}
+            loss_val, pred_val = sess.run([ops['loss'], ops['pred']],
+                                      feed_dict=feed_dict)
+            batch_pred_sum += pred_val
+            batch_pred_val = np.argmax(pred_val, 1)
+            for el_idx in range(cur_batch_size):
+                batch_pred_classes[el_idx, batch_pred_val[el_idx]] += 1
+            batch_loss_sum += (loss_val * cur_batch_size / float(num_votes))
+        pred_val_topk = np.argsort(batch_pred_sum, axis=-1)[:,-1*np.array(range(topk))-1]
+        pred_val = np.argmax(batch_pred_classes, 1)
+        pred_val = np.argmax(batch_pred_sum, 1)
+        predValues.append(np.ndarray.tolist(pred_val)[0])
+        # Aggregating END
 
-            # Aggregating BEG
-            batch_loss_sum = 0 # sum of losses for the batch
-            batch_pred_sum = np.zeros((cur_batch_size, NUM_CLASSES)) # score for classes
-            batch_pred_classes = np.zeros((cur_batch_size, NUM_CLASSES)) # 0/1 for classes
-            for vote_idx in range(num_votes):
-                rotated_data = provider.rotate_point_cloud_by_angle(current_data[start_idx:end_idx, :, :],
-                                                  vote_idx/float(num_votes) * np.pi * 2)
-                feed_dict = {ops['pointclouds_pl']: rotated_data,
-                             ops['labels_pl']: current_label[start_idx:end_idx],
-                             ops['is_training_pl']: is_training}
-                loss_val, pred_val = sess.run([ops['loss'], ops['pred']],
-                                          feed_dict=feed_dict)
-        
-                batch_pred_sum += pred_val
-                batch_pred_val = np.argmax(pred_val, 1)
-                for el_idx in range(cur_batch_size):
-                    batch_pred_classes[el_idx, batch_pred_val[el_idx]] += 1
-                batch_loss_sum += (loss_val * cur_batch_size / float(num_votes))
-            pred_val_topk = np.argsort(batch_pred_sum, axis=-1)[:,-1*np.array(range(topk))-1]
-            pred_val = np.argmax(batch_pred_classes, 1)
-            pred_val = np.argmax(batch_pred_sum, 1)
-            predValues.append(np.ndarray.tolist(pred_val)) 
-            # Aggregating END
-            correct = np.sum(pred_val == current_label[start_idx:end_idx])
-            #db.set_trace()
+        correct = np.sum(pred_val == current_label[start_idx:end_idx])
+        # correct = np.sum(pred_val_topk[:,0:topk] == label_val)
+        total_correct += correct
+        total_seen += cur_batch_size
+        loss_sum += batch_loss_sum
 
-            # correct = np.sum(pred_val_topk[:,0:topk] == label_val)
-            total_correct += correct
-            total_seen += cur_batch_size
-            loss_sum += batch_loss_sum
+        for i in range(start_idx, end_idx):
+            l = current_label[i]
+            total_seen_class[l] += 1
+            total_correct_class[l] += (pred_val[i-start_idx] == l)
+            fout.write('%d, %d\n' % (pred_val[i-start_idx], l))
 
-            for i in range(start_idx, end_idx):
-                l = current_label[i]
-                total_seen_class[l] += 1
-                total_correct_class[l] += (pred_val[i-start_idx] == l)
-                fout.write('%d, %d\n' % (pred_val[i-start_idx], l))
+            if pred_val[i-start_idx] != l and FLAGS.visu: # ERROR CASE, DUMP!
+                img_filename = '%d_label_%s_pred_%s.jpg' % (error_cnt, SHAPE_NAMES[l],
+                                                       SHAPE_NAMES[pred_val[i-start_idx]])
+                img_filename = os.path.join(DUMP_DIR, img_filename)
+                output_img = pc_util.point_cloud_three_views(np.squeeze(current_data[i, :, :]))
+                scipy.misc.imsave(img_filename, output_img)
+                error_cnt += 1
 
-                if pred_val[i-start_idx] != l and FLAGS.visu: # ERROR CASE, DUMP!
-                    img_filename = '%d_label_%s_pred_%s.jpg' % (error_cnt, SHAPE_NAMES[l],
-                                                           SHAPE_NAMES[pred_val[i-start_idx]])
-                    img_filename = os.path.join(DUMP_DIR, img_filename)
-                    output_img = pc_util.point_cloud_three_views(np.squeeze(current_data[i, :, :]))
-                    scipy.misc.imsave(img_filename, output_img)
-                    error_cnt += 1
-    #pdb.set_trace()
     log_string('eval mean loss: %f' % (loss_sum / float(total_seen)))
     log_string('eval accuracy: %f' % (total_correct / float(total_seen)))
     log_string('eval avg class acc: %f' % (np.mean(np.array(total_correct_class)/np.array(total_seen_class,dtype=np.float))))
-    
-    
+
     class_accuracies = np.array(total_correct_class)/np.array(total_seen_class,dtype=np.float)
     for i, name in enumerate(SHAPE_NAMES):
         log_string('%10s:\t%0.3f' % (name, class_accuracies[i]))
+    return predValues
 
+canvas = scene.SceneCanvas(keys='interactive', show=True)
+view = canvas.central_widget.add_view()
+fov = 60.
+cam1 = scene.cameras.FlyCamera(parent=view.scene, fov=fov)
+cam2 = scene.cameras.TurntableCamera(parent=view.scene, fov=fov)
+cam3 = scene.cameras.ArcballCamera(parent=view.scene, fov=fov)
+view.camera = cam1
+
+# Implement key presses
+@canvas.events.key_press.connect
+def on_key_press(event):
+    global opaque_cmap, translucent_cmap
+    if event.text == '1':
+        cam_toggle = {cam1: cam2, cam2: cam3, cam3: cam1}
+        view.camera = cam_toggle.get(view.camera, 'fly')
+    elif event.text == '2':
+        methods = ['mip', 'translucent', 'iso', 'additive']
+        method = methods[(methods.index(volume1.method) + 1) % 4]
+        print("Volume render method: %s" % method)
+        cmap = opaque_cmap if method in ['mip', 'iso'] else translucent_cmap
+        volume1.method = method
+        volume1.cmap = cmap
+        volume2.method = method
+        volume2.cmap = cmap
+    elif event.text == '3':
+        volume1.visible = not volume1.visible
+        volume2.visible = not volume1.visible
+    elif event.text == '4':
+        if volume1.method in ['mip', 'iso']:
+            cmap = opaque_cmap = next(opaque_cmaps)
+        else:
+            cmap = translucent_cmap = next(translucent_cmaps)
+        volume1.cmap = cmap
+        volume2.cmap = cmap
+    elif event.text == '0':
+        cam1.set_range()
+        cam3.set_range()
+    elif event.text != '' and event.text in '[]':
+        s = -0.025 if event.text == '[' else 0.025
+        volume1.threshold += s
+        volume2.threshold += s
+        th = volume1.threshold if volume1.visible else volume2.threshold
+        print("Isosurface threshold: %0.3f" % th)
 
 
 if __name__=='__main__':
     with tf.Graph().as_default():
-        evaluate(num_votes=1)
+        predValues = evaluate(num_votes=1)
+
+    data,labels = retrieveData()    
+    array = convertToNumpy2D(data)
+    scatter = scene.visuals.Markers()
+    scatter.set_data(array[:,:3], face_color=colorMaping(predValues,data),size=1)
+    view.add(scatter)
+    view.camera = scene.PanZoomCamera(aspect=1)
+    view.camera.set_range()
+    app.run()
     LOG_FOUT.close()
 
